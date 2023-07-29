9a85cd7b1781be55ebc3bd54995e0f36208227c5
br0steen <osteensco@gmail.com>
2023-07-28 19:45:59 -0500

    websocket components

--- main.go
+++ main.go
@@ -1,12 +1,61 @@
 package main
 
 import (
+	"fmt"
+	"io"
+
 	"github.com/aofei/air"
+	"golang.org/x/net/websocket"
 )
 
+type Server struct {
+	conns map[*websocket.Conn]bool
+}
+
+func NewServer() *Server {
+	// creates a server and opens websocket connections among clients
+	return &Server{
+		conns: make(map[*websocket.Conn]bool),
+	}
+}
+
+func (s *Server) handleWS(ws *websocket.Conn) {
+	fmt.Printf("incoming connection from client: %v", ws.RemoteAddr())
+
+	s.conns[ws] = true
+
+	s.readLoop(ws)
+}
+
+func (s *Server) readLoop(ws *websocket.Conn) {
+	buf := make([]byte, 1024)
+	for {
+		n, err := ws.Read(buf)
+		if err != nil {
+			if err == io.EOF {
+				break
+			}
+			fmt.Println("read error: ", err)
+			continue
+		}
+		msg := buf[:n]
+		fmt.Println(string(msg))
+
+	}
+}
+
+func indexHandler(req *air.Request, res *air.Response) error {
+	println("yes")
+	return res.Render(nil, "/static/index.html")
+}
+
 func main() {
-	air.Default.GET("/", func(req *air.Request, res *air.Response) error {
-		return res.WriteString("Hello world")
-	})
-	air.Default.Serve()
+
+	// server := NewServer()
+
+	app := air.New()
+
+	app.GET("/", indexHandler)
+
+	app.Serve()
 }
