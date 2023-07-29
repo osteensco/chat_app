132171891e1d06b8245c404383fb2335f2ff462c
br0steen <osteensco@gmail.com>
2023-07-29 18:34:04 -0500

    chatroom success

--- static/script.js
+++ static/script.js
@@ -4,27 +4,35 @@
 function sendMessage(conn) {
     var newmessage = document.getElementById("message");
     if (newmessage != null) {
-        console.log(newmessage);
-
         conn.send(newmessage.value);
+        newmessage.value = "";
     }
-    console.log("error sending message")
     return false;
 }
 
+function receiveMessage(message) {
+    var chatbox = document.getElementById("chatmessages");
+    console.log(message);
+    var newMessage = message.data;
+    chatbox.value += "\n" + newMessage;
+    chatbox.scrollTop = chatbox.scrollHeight;
 
+}
 
 window.onload = function () {
 
     if (window["WebSocket"]) {
         console.log("browser websocket support found");
-        conn = new WebSocket("ws://" + document.location.host + "/");
+        conn = new WebSocket("ws://" + document.location.host + "/ws");
 
-        document.getElementById("chatroom-message").onsubmit = () => {console.log("error"); sendMessage(conn);};
+        document.getElementById("chatroom-message").onsubmit = (event) => {
+            event.preventDefault();
+            sendMessage(conn);
+        };
         
         conn.onmessage = (message) => {
-            console.log(message);
-        }
+            receiveMessage(message)
+        };
 
     } else {
         alert("Websockets not supported by browser!");
