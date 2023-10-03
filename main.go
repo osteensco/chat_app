432cde6fa7b6b6e7aaf1af8ff1c4b4193bda0f0b
br0steen <osteensco@gmail.com>
2023-10-02 19:24:51 -0500

    TODO

--- main.go
+++ main.go
@@ -43,6 +43,10 @@ func main() {
 }
 
 // TODO
+//	 - ${userName} has entered chatroom message
+//	 - ${userName} has left chatroom message
+//	 - remove chatrooms and messages from DB after 10 min of inactivity
+//		 - use go routine chatroom method to start a timer when clientList becomes empty
 //	 - add cockroachDB functions
 
 // bugs
