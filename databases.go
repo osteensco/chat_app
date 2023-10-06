212669902006e1bfc00f8ede0084c68cc7b51e4c
br0steen <osteensco@gmail.com>
2023-10-06 15:21:21 -0500

    started cockroachDB functions

--- databases.go
+++ databases.go
@@ -43,14 +43,6 @@ func addUserToChatroomRedis(ctx context.Context, client *redis.Client, displayNa
 	return err
 }
 
-// func getChatroomUsersRedis(ctx context.Context, client *redis.Client, chatroomPath string) ([]string, error) {
-// 	members, err := client.SMembers(ctx, "chatroom:"+chatroomPath).Result()
-// 	if err != nil {
-// 		return nil, err
-// 	}
-// 	return members, nil
-// }
-
 func isUserInChatroomRedis(ctx context.Context, client *redis.Client, displayname string, chatroomPath string) (bool, error) {
 	isMember, err := client.SIsMember(ctx, "users_"+chatroomPath, displayname).Result()
 	if err != nil {
@@ -165,3 +157,114 @@ func connectCockrochDB(context context.Context) *pgx.Conn {
 	return conn
 
 }
+
+// func addUserToChatroomCRDB(ctx context.Context, client *pgx.Conn, displayName string, chatroomPath string) error {
+// 	_, err := client.SAdd(ctx, "users_"+chatroomPath, displayName).Result()
+// 	if err != nil {
+// 		log.Println("Error adding user to chatroom:", err)
+// 	}
+// 	return err
+// }
+
+// func isUserInChatroomCRDB(ctx context.Context, client *pgx.Conn, displayname string, chatroomPath string) (bool, error) {
+// 	isMember, err := client.SIsMember(ctx, "users_"+chatroomPath, displayname).Result()
+// 	if err != nil {
+// 		return false, err
+// 	}
+
+// 	return isMember, nil
+// }
+
+// func removeUserFromChatroomCRDB(ctx context.Context, client *pgx.Conn, displayName string, chatroomPath string) error {
+// 	_, err := client.SRem(ctx, "users_"+chatroomPath, displayName).Result()
+// 	if err != nil {
+// 		log.Println("Error removing user from chatroom:", err)
+// 	}
+// 	return err
+// }
+
+// func changeUserNameCRDB(ctx context.Context, client *pgx.Conn, oldName string, newName string, chatroomPath string) error {
+
+// 	userExists, err := isUserInChatroomRedis(ctx, client, oldName, chatroomPath)
+
+// 	if userExists && err == nil {
+// 		err := removeUserFromChatroomRedis(ctx, client, oldName, chatroomPath)
+// 		if err != nil {
+// 			return err
+// 		} else {
+// 			err := addUserToChatroomRedis(ctx, client, newName, chatroomPath)
+// 			if err != nil {
+// 				return err
+// 			}
+// 		}
+// 	} else if !userExists {
+// 		log.Panicf("%v not found in Redis db", oldName)
+// 	}
+
+// 	return err
+
+// }
+
+// func getMessageHistoryCRDB(ctx context.Context, client *pgx.Conn, chatroomPath string) ([]string, error) {
+// 	chatMessages, err := client.LRange(ctx, "messages_"+chatroomPath, 0, -1).Result()
+// 	if err != nil {
+// 		log.Println("Error getting message history from chatroom:", err)
+// 	}
+// 	return chatMessages, err
+// }
+
+// func getMessageHistoryLengthCRDB(ctx context.Context, client *pgx.Conn, chatroomPath string) (int64, error) {
+// 	length, err := client.LLen(ctx, "messages_"+chatroomPath).Result()
+// 	if err != nil {
+// 		log.Println("Error getting length of message history from chatroom:", err)
+// 	}
+// 	return length, err
+// }
+
+// func addMessageToHistoryCRDB(ctx context.Context, client *pgx.Conn, chatroomPath string, chatMessage string) error {
+// 	_, err := client.RPush(ctx, "messages_"+chatroomPath, chatMessage).Result()
+// 	if err != nil {
+// 		log.Println("Error adding message to chatroom history:", err)
+// 	}
+// 	return err
+// }
+
+// func removeMessageFromHistoryCRDB(ctx context.Context, client *pgx.Conn, chatroomPath string) error {
+// 	_, err := client.LPop(ctx, "messages_"+chatroomPath).Result()
+// 	if err != nil {
+// 		log.Println("Error removing message from chatroom history:", err)
+// 	}
+// 	return err
+// }
+
+// func deleteKeyCRDB(ctx context.Context, client *pgx.Conn, key string) error {
+// 	_, err := client.Del(ctx, "messages_"+key).Result()
+// 	if err != nil {
+// 		log.Println("Error deleting message history of chatroom:", err)
+// 	}
+// 	return err
+// }
+
+// func getAllChatroomsCRDB(ctx context.Context, client *pgx.Conn, key string) (map[string]string, error) {
+// 	rooms, err := client.HGetAll(ctx, key).Result()
+// 	if err != nil {
+// 		log.Println("Error getting all chatrooms in lobby:", err)
+// 	}
+// 	return rooms, err
+// }
+
+// func addChatroomToLobbyCRDB(ctx context.Context, client *pgx.Conn, key string, room map[string]interface{}) error {
+// 	_, err := client.HSet(ctx, key, room).Result()
+// 	if err != nil {
+// 		log.Println("Error adding chatroom to lobby:", err)
+// 	}
+// 	return err
+// }
+
+// func removeChatroomFromLobbyCRDB(ctx context.Context, client *pgx.Conn, key string, roomname string) error {
+// 	_, err := client.HDel(ctx, key, roomname).Result()
+// 	if err != nil {
+// 		log.Println("Error removing chatroom from lobby:", err)
+// 	}
+// 	return err
+// }
